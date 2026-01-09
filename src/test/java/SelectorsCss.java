1ca14f7e345086e74daec927be00d61c5ba4e1cb
adfugamspes-JSFEPRESCHOOL2024Q2 <report.shor@gmail.com>
2026-01-09 11:58:53 +0200

    Start

---
+++ src/test/java/SelectorsCss.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SelectorsCss {
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void phoneBookTest() {
+    driver.manage().window().maximize();
+    driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//    driver.get("https://telranedu.web.app/home");
+    pause(3);
+    driver.navigate().to("https://telranedu.web.app/home");
+    pause(3);
+    WebElement btnAbout = driver.findElement(By.cssSelector("a[href='/about']"));
+    btnAbout.click();
+//    WebElement divRoot = driver.findElement(By.cssSelector("div[id='root']"));
+//    WebElement divRoot = driver.findElement(By.cssSelector("div[id='root']"));
+    WebElement divRootClass = driver.findElement(By.cssSelector("div[class='container']"));
+        System.out.println(divRootClass.getAttribute("id"));
+        WebElement divRoot = driver.findElement(By.cssSelector("#root"));
+        System.out.println(divRoot.getAttribute("class"));
+        pause(3);
+    driver.navigate().to("https://telranedu.web.app/login");
+        pause(3);
+        driver.navigate().back();
+        driver.close();
+        driver.quit();
+    }
+
+    static void pause(int time) {
+        try {
+            Thread.sleep(time*1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
