65178ba85830d36751d27f13d5267d6d3b95c160
adfugamspes-JSFEPRESCHOOL2024Q2 <report.shor@gmail.com>
2026-01-11 12:50:59 +0200

    Commit2

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -2,6 +2,7 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Bye, world");
+        System.out.println("Commit test");
 
     }
 }
