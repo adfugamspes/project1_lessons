da41009b10b265cd9aec611d8db45641c44576a8
adfugamspes-JSFEPRESCHOOL2024Q2 <report.shor@gmail.com>
2026-01-13 20:46:51 +0200

    CW3

--- src/test/java/SelectorsCss.java
+++ src/test/java/SelectorsCss.java
@@ -50,32 +50,84 @@ public class SelectorsCss {
     }
 
     @Test
-    public void ilCarroTest () {
+    public void ilCarroTest1() {
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.navigate().to("https://ilcarro.web.app/search");
+        driver.get("https://ilcarro.web.app/search");
         pause(3);
-        WebElement btnSearch = driver.findElement(By.cssSelector("a[href='/search']"));
+        WebElement logoHeader = driver.findElement(By.cssSelector("img[alt='logo']"));
+        logoHeader.click();
+        pause(3);
+//        a.navigation-link
+//        a[class='navigation-link']
+//        *[class='navigation-link']
+//        [class='navigation-link']
+        WebElement btnSearch = driver.findElement(By.className("navigation-link"));
         btnSearch.click();
         pause(3);
-        WebElement btnLet = driver.findElement(By.cssSelector("a[href='/let-car-work']"));
+//        a[id='1']
+//        a#1.navigation-link
+//        #1
+//        [id='1']
+//        WebElement btnLet = driver.findElement(By.cssSelector("a[id='1']"));
+//        WebElement btnLet = driver.findElement(By.id("1");
+//        WebElement btnLet = driver.findElement(By.linkText("Let the car work"));
+        WebElement btnLet = driver.findElement(By.partialLinkText("work"));
         btnLet.click();
         pause(3);
-        WebElement btnTerms = driver.findElement(By.cssSelector("a[href='/terms-of-use']"));
+
+        // a[href='/terms-of-use']
+        // a[href*='/terms'] - содержит
+        // a[href^='/terms'] - начинается
+        // a[href$='-use'] - заканчивается
+        //a.navigation-link[href='/terms-of-use']
+//        WebElement btnTerms = driver.findElement(By.cssSelector("a[href='/terms-of-use']"));
+        WebElement btnTerms = driver.findElement(By.cssSelector("a[href*='/terms']"));
         btnTerms.click();
         pause(3);
-        WebElement btnSignUp = driver.findElement(By.xpath("/html/body/app-root/app-navigator/div[1]/a[5]"));
+
+        WebElement btnSignUp = driver.findElement(By.cssSelector("div.header a.navigation-link:nth-child(5)"));
         btnSignUp.click();
         pause(3);
-        WebElement btnLogIn = driver.findElement(By.xpath("/html/body/app-root/app-navigator/div[1]/a[6]"));
+
+//        WebElement btnLogIn = driver.findElement(By.cssSelector("div.header a.navigation-link:nth-child(6)"));
+        WebElement btnLogIn = driver.findElement(By.cssSelector("div.header a.navigation-link:last-child"));
         btnLogIn.click();
         pause(3);
-        WebElement btnLogo = driver.findElement(By.cssSelector("a[class='logo']"));
-        btnLogo.click();
-        pause(3);
-        driver.navigate().refresh();
+
+        driver.navigate().to("https://ilcarro.web.app/search");
         pause(3);
         driver.quit();
     }
+
+//    @Test
+//    public void ilCarroTest () {
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.navigate().to("https://ilcarro.web.app/search");
+//        pause(3);
+//        WebElement btnSearch = driver.findElement(By.cssSelector("a[href='/search']"));
+//        btnSearch.click();
+//        pause(3);
+//        WebElement btnLet = driver.findElement(By.cssSelector("a[href='/let-car-work']"));
+//        btnLet.click();
+//        pause(3);
+//        WebElement btnTerms = driver.findElement(By.cssSelector("a[href='/terms-of-use']"));
+//        btnTerms.click();
+//        pause(3);
+//        WebElement btnSignUp = driver.findElement(By.xpath("/html/body/app-root/app-navigator/div[1]/a[5]"));
+//        btnSignUp.click();
+//        pause(3);
+//        WebElement btnLogIn = driver.findElement(By.xpath("/html/body/app-root/app-navigator/div[1]/a[6]"));
+//        btnLogIn.click();
+//        pause(3);
+//        WebElement btnLogo = driver.findElement(By.cssSelector("a[class='logo']"));
+//        btnLogo.click();
+//        pause(3);
+//        driver.navigate().refresh();
+//        pause(3);
+//        driver.quit();
+//    }
 }
